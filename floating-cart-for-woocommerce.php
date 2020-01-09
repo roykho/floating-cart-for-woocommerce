e8c4a7f3447d4917eafddc8cb457c9aac22068bf
roykho <roykho77@gmail.com>
2020-01-09 10:42:29 -0800

    Bumping version

--- floating-cart-for-woocommerce.php
+++ floating-cart-for-woocommerce.php
@@ -3,7 +3,8 @@
  * Plugin Name: WooCommerce Bookings
  * Plugin URI: https://woocommerce.com/products/woocommerce-bookings/
  * Description: Setup bookable products such as for reservations, services and hires.
- * Version: 2.3.18
+ * Version: 2.3.19
+
 
 
 
@@ -11,8 +12,8 @@
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce-bookings
  * Domain Path: /languages
- * Tested up to: 2.6
- * WC tested up to: 3.5
+ * Tested up to: 1.3
+ * WC tested up to: 2.2
  * WC requires at least: 2.6
  *
  * License: GNU General Public License v3.0
@@ -95,7 +96,7 @@ function woocommerce_bookings_activate() {
 
 if ( ! class_exists( 'WC_Bookings' ) ) :
 
-	define( 'WC_BOOKINGS_VERSION', '2.3.16' );
+	define( 'WC_BOOKINGS_VERSION', '2.3.19' );
 
 	define( 'WC_BOOKINGS_TEMPLATE_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/templates/' );
 	define( 'WC_BOOKINGS_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
